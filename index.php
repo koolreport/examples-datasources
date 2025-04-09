d3270d543272b50ad5cb9f2a1854f437fbef49b4
dongnl <nguyenluongdong@gmail.com>
2025-04-09 15:30:09 +0700

    update link in index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <a id="baseUrl" class="navbar-brand" href="https://www.koolreport.com/examples">KoolReport Examples</a>
-        <a id="repoUrl" class="navbar-brand" href="https://github.com/koolreport/examples-processes">GitHub</a>
+        <a id="repoUrl" class="navbar-brand" href="https://github.com/koolreport/examples-datasources">GitHub</a>
         <ul id="topMenu" class="navbar-nav mr-auto">
 
         </ul>
